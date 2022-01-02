e638daea6744b43a33d539d9159f0523c5cd8c04
Kevin Bader <kevinbader@users.noreply.github.com>
2022-01-02 20:03:19 +0100

    rs d8 p2

--- src/bin/day8.rs
+++ src/bin/day8.rs
@@ -1,157 +1,96 @@
-use std::{collections::HashMap, fmt::Display, fs};
+use itertools::Itertools;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let input = fs::read_to_string("./input/day8.txt").expect("failed to read input file");
-    let entries: Vec<Entry> = parse(&input);
-    let mut total = 0;
-    for entry in &entries {
-        let decoder = Decoder::from(entry);
-        let ones = decoder.count_in_output(Digit::One);
-        let fours = decoder.count_in_output(Digit::Four);
-        let sevens = decoder.count_in_output(Digit::Seven);
-        let eights = decoder.count_in_output(Digit::Eight);
-        let sum = ones + fours + sevens + eights;
-        total += sum;
-        println!("{}", decoder);
-        println!("sum: {}", sum);
-    }
-    println!();
+    let total: u32 = input
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(decode_output)
+        .sum();
     println!("total: {}", total);
 }
 
-fn parse(input: &str) -> Vec<Entry> {
-    input
-        .lines()
-        .map(|l| l.trim())
-        .filter(|l| l.len() > 0)
-        .map(Entry::from_str)
-        .collect()
-}
-
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
-struct SignalPattern(String);
+struct Pattern(String);
 
-impl SignalPattern {
+impl Pattern {
     fn from(word: &str) -> Self {
         let mut chars: Vec<_> = word.to_lowercase().chars().collect();
         chars.sort_unstable();
         Self(chars.iter().collect())
     }
-}
-
-struct Entry {
-    input: Vec<SignalPattern>,
-    output: Vec<SignalPattern>,
-}
-
-impl Entry {
-    fn from_str(line: &str) -> Self {
-        let (input, output) = match line.split('|').collect::<Vec<&str>>().as_slice() {
-            [input, output] => (*input, *output),
-            split => panic!("unexpected format: {:?}", split),
-        };
-        let input: Vec<SignalPattern> = input
-            .split_whitespace()
-            .map(|x| SignalPattern::from(x))
-            .collect();
-        assert_eq!(input.len(), 10);
-        let output: Vec<SignalPattern> = output
-            .split_whitespace()
-            .map(|x| SignalPattern::from(x))
-            .collect();
-        assert_eq!(output.len(), 4);
-        Entry { input, output }
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+    fn contains(&self, other: &Self) -> bool {
+        other.0.chars().all(|c| self.0.contains(c))
     }
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-enum Digit {
-    Zero,
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-}
+fn decode_output(line: &str) -> u32 {
+    let (input, output) = line.split('|').collect_tuple().unwrap();
+    let input = input.split_whitespace().map(Pattern::from).collect_vec();
+    let output = output.split_whitespace().map(Pattern::from).collect_vec();
 
-impl Digit {
-    fn from_str(signal_pattern: &SignalPattern) -> Option<Self> {
-        use Digit::*;
-        // Some digits are displayed using a unique number of segments:
-        match signal_pattern.0.len() {
-            2 => Some(One),
-            4 => Some(Four),
-            3 => Some(Seven),
-            7 => Some(Eight),
-            _ => None,
-        }
-    }
-}
+    let one = input.iter().find(|x| x.len() == 2).unwrap();
+    let four = input.iter().find(|x| x.len() == 4).unwrap();
+    let seven = input.iter().find(|x| x.len() == 3).unwrap();
+    let eight = input.iter().find(|x| x.len() == 7).unwrap();
+    // 6 is the only pattern of length 6 that doesn't contain 1 or 4.
+    let six = input
+        .iter()
+        .filter(|x| x.len() == 6)
+        .find(|x| !x.contains(one) && !x.contains(four))
+        .unwrap();
+    // 9 is the only pattern of length 6 that contains 4.
+    let nine = input
+        .iter()
+        .filter(|x| x.len() == 6)
+        .find(|x| x.contains(four))
+        .unwrap();
+    // 0 is the only pattern of length 6 that contains 1 but not 4.
+    let zero = input
+        .iter()
+        .filter(|x| x.len() == 6)
+        .find(|x| x.contains(one) && !x.contains(four))
+        .unwrap();
+    // 3 is the only pattern of length 5 that contains 1.
+    let three = input
+        .iter()
+        .filter(|x| x.len() == 5)
+        .find(|x| x.contains(one))
+        .unwrap();
+    // 2 is the only pattern of length 5 that doesn't contain 1 and also doesn't fit into 6.
+    let two = input
+        .iter()
+        .filter(|x| x.len() == 5)
+        .find(|x| !x.contains(one) && !six.contains(x))
+        .unwrap();
+    // 5 is the only pattern of length 5 that doesn't contain 1 and fits into 6.
+    let five = input
+        .iter()
+        .filter(|x| x.len() == 5)
+        .find(|x| !x.contains(one) && six.contains(x))
+        .unwrap();
 
-struct Decoder {
-    pattern_to_digit: HashMap<SignalPattern, Digit>,
-    input_digit_counts: HashMap<Digit, usize>,
-    output_digit_counts: HashMap<Digit, usize>,
-}
+    let patterns = HashMap::from([
+        (zero, 0),
+        (one, 1),
+        (two, 2),
+        (three, 3),
+        (four, 4),
+        (five, 5),
+        (six, 6),
+        (seven, 7),
+        (eight, 8),
+        (nine, 9),
+    ]);
 
-impl Decoder {
-    fn from(entry: &Entry) -> Self {
-        let mut decoder = Self {
-            pattern_to_digit: HashMap::new(),
-            input_digit_counts: HashMap::new(),
-            output_digit_counts: HashMap::new(),
-        };
-        for input_pattern in &entry.input {
-            if let Some(digit) = decoder.add(input_pattern) {
-                *decoder.input_digit_counts.entry(digit).or_insert(0) += 1;
-            }
-        }
-        for output_pattern in &entry.output {
-            if let Some(digit) = decoder.add(output_pattern) {
-                *decoder.output_digit_counts.entry(digit).or_insert(0) += 1;
-            }
-        }
-        decoder
-    }
+    let output_value: String = output.iter().map(|p| patterns[&p].to_string()).collect();
 
-    fn add(&mut self, signal_pattern: &SignalPattern) -> Option<Digit> {
-        if self.pattern_to_digit.contains_key(signal_pattern) {
-            Some(self.pattern_to_digit[signal_pattern])
-        } else if let Some(digit) = Digit::from_str(signal_pattern) {
-            self.pattern_to_digit.insert(signal_pattern.clone(), digit);
-            Some(digit)
-        } else {
-            None
-        }
-    }
-
-    fn count_in_input(&self, digit: Digit) -> usize {
-        *self.input_digit_counts.get(&digit).unwrap_or(&0)
-    }
-
-    fn count_in_output(&self, digit: Digit) -> usize {
-        *self.output_digit_counts.get(&digit).unwrap_or(&0)
-    }
-}
-
-impl Display for Decoder {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut lines = vec![];
-        for (pattern, digit) in &self.pattern_to_digit {
-            lines.push(format!(
-                "{:>7} => {:?} (occurrences: input={} output={})",
-                pattern.0,
-                digit,
-                self.count_in_input(*digit),
-                self.count_in_output(*digit)
-            ));
-        }
-        write!(f, "{}", lines.join("\n"))
-    }
+    output_value.parse().unwrap()
 }
 
 #[cfg(test)]
@@ -172,20 +111,21 @@ mod test {
     ";
 
     #[test]
-    fn example() {
-        let entries: Vec<Entry> = parse(INPUT);
-        let mut total = 0;
-        for entry in &entries {
-            let decoder = Decoder::from(entry);
-            let ones = decoder.count_in_output(Digit::One);
-            let fours = decoder.count_in_output(Digit::Four);
-            let sevens = decoder.count_in_output(Digit::Seven);
-            let eights = decoder.count_in_output(Digit::Eight);
-            let sum = ones + fours + sevens + eights;
-            total += sum;
-            println!("{}", decoder);
-            println!("sum: {}", sum);
-        }
-        assert_eq!(total, 26);
+    fn example_part2() {
+        let output_total: u32 = INPUT
+            .lines()
+            .map(|l| l.trim())
+            .filter(|l| !l.is_empty())
+            .map(decode_output)
+            .sum();
+        assert_eq!(output_total, 61229);
+    }
+
+    #[test]
+    fn test_single_entry() {
+        let line =
+            "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf";
+        let output = decode_output(line);
+        assert_eq!(output, 5353);
     }
 }
