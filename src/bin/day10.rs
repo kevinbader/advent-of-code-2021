355034e731b1d764b72249d8e45cad2666941d21
Kevin Bader <kevinbader@users.noreply.github.com>
2022-01-08 17:22:27 +0100

    Refactor in prep for part2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -6,22 +6,28 @@ fn main() {
     println!("score: {}", score);
 }
 
+enum ParseLineResult {
+    Ok,
+    Incomplete,
+    SyntaxError(SyntaxError),
+}
+impl ParseLineResult {
+    fn syntax_error(pos: usize, expected: char, actual: Option<char>) -> Self {
+        Self::SyntaxError(SyntaxError {
+            pos,
+            expected,
+            actual,
+        })
+    }
+}
+
 struct SyntaxError {
     pos: usize,
     expected: char,
     actual: Option<char>,
 }
-impl SyntaxError {
-    fn new(pos: usize, expected: char, actual: Option<char>) -> Self {
-        Self {
-            pos,
-            expected,
-            actual,
-        }
-    }
-}
 
-fn find_first_syntax_error_in(line: &str) -> Option<SyntaxError> {
+fn parse_line(line: &str) -> ParseLineResult {
     let mut stack = vec![];
     for (pos, symbol) in line.chars().enumerate() {
         match symbol {
@@ -31,18 +37,29 @@ fn find_first_syntax_error_in(line: &str) -> Option<SyntaxError> {
                 let expected = opening_symbol_for(symbol).unwrap();
                 if let Some(corresponding_symbol) = stack.pop() {
                     if corresponding_symbol != expected {
-                        return Some(SyntaxError::new(pos, expected, Some(actual)));
+                        return ParseLineResult::syntax_error(pos, expected, Some(actual));
                     }
                 } else {
                     // Close symbol but no corresponding opening symbol - that's an error.
-                    return Some(SyntaxError::new(pos, expected, Some(actual)));
+                    return ParseLineResult::syntax_error(pos, expected, Some(actual));
                 }
             }
             // Ignoring any other character:
             _ => {}
         };
     }
-    None
+    if stack.is_empty() {
+        ParseLineResult::Ok
+    } else {
+        ParseLineResult::Incomplete
+    }
+}
+
+fn first_syntax_error_in(line: &str) -> Option<SyntaxError> {
+    match parse_line(line) {
+        ParseLineResult::SyntaxError(syntax_error) => Some(syntax_error),
+        _ => None,
+    }
 }
 
 fn opening_symbol_for(symbol: char) -> Option<char> {
@@ -56,9 +73,7 @@ fn opening_symbol_for(symbol: char) -> Option<char> {
 }
 
 fn syntax_errors_in(text: &str) -> Vec<SyntaxError> {
-    text.lines()
-        .filter_map(find_first_syntax_error_in)
-        .collect()
+    text.lines().filter_map(first_syntax_error_in).collect()
 }
 
 fn score_for(syntax_errors: &[SyntaxError]) -> usize {
