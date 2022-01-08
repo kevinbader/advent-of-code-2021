db1afcf832c78394447a820a766be394f4274552
Kevin Bader <kevinbader@users.noreply.github.com>
2022-01-08 18:08:31 +0100

    rs d10 p2

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -3,15 +3,18 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("./input/day10.txt").expect("failed to read input file");
     let score = score_for(&syntax_errors_in(&input));
-    println!("score: {}", score);
+    println!("part 1 syntax error score: {}", score);
+    let incomplete_lines = incomplete_lines_in(&input);
+    let completion_score = completion_score_of(&incomplete_lines);
+    println!("part 2 completion score: {}", completion_score);
 }
 
-enum ParseLineResult {
+enum ParseLineResult<'a> {
     Ok,
-    Incomplete,
+    Incomplete(IncompleteLine<'a>),
     SyntaxError(SyntaxError),
 }
-impl ParseLineResult {
+impl<'a> ParseLineResult<'a> {
     fn syntax_error(pos: usize, expected: char, actual: Option<char>) -> Self {
         Self::SyntaxError(SyntaxError {
             pos,
@@ -19,12 +22,48 @@ impl ParseLineResult {
             actual,
         })
     }
+    fn incomplete_line(line: &'a str, open_symbols: Vec<char>) -> Self {
+        Self::Incomplete(IncompleteLine { line, open_symbols })
+    }
 }
 
 struct SyntaxError {
     pos: usize,
     expected: char,
     actual: Option<char>,
+}
+
+struct IncompleteLine<'a> {
+    line: &'a str,
+    open_symbols: Vec<char>,
+}
+impl<'a> IncompleteLine<'a> {
+    fn completed(&self) -> String {
+        format!("{}{}", self.line, self.completion())
+    }
+    fn score(&self) -> usize {
+        let mut score = 0;
+        for c in self.completion().chars() {
+            // For each character, multiply the total score by 5..
+            score *= 5;
+            // ..and then increase it by the character's value.
+            score += match c {
+                ')' => 1,
+                ']' => 2,
+                '}' => 3,
+                '>' => 4,
+                _ => panic!("no score defined for char used for completion: {}", c),
+            };
+        }
+        score
+    }
+    fn completion(&self) -> String {
+        self.open_symbols
+            .iter()
+            .rev()
+            .filter_map(|c| closing_symbol_for(*c))
+            .collect::<String>()
+    }
 }
 
 fn parse_line(line: &str) -> ParseLineResult {
@@ -51,14 +90,7 @@ fn parse_line(line: &str) -> ParseLineResult {
     if stack.is_empty() {
         ParseLineResult::Ok
     } else {
-        ParseLineResult::Incomplete
-    }
-}
-
-fn first_syntax_error_in(line: &str) -> Option<SyntaxError> {
-    match parse_line(line) {
-        ParseLineResult::SyntaxError(syntax_error) => Some(syntax_error),
-        _ => None,
+        ParseLineResult::incomplete_line(line, stack)
     }
 }
 
@@ -72,10 +104,39 @@ fn opening_symbol_for(symbol: char) -> Option<char> {
     }
 }
 
-fn syntax_errors_in(text: &str) -> Vec<SyntaxError> {
-    text.lines().filter_map(first_syntax_error_in).collect()
+fn closing_symbol_for(symbol: char) -> Option<char> {
+    match symbol {
+        '(' => Some(')'),
+        '[' => Some(']'),
+        '{' => Some('}'),
+        '<' => Some('>'),
+        _ => None,
+    }
 }
 
+// Part 1
+fn syntax_errors_in(text: &str) -> Vec<SyntaxError> {
+    text.lines()
+        .map(|l| l.trim())
+        .filter_map(|line| match parse_line(line) {
+            ParseLineResult::SyntaxError(syntax_error) => Some(syntax_error),
+            _ => None,
+        })
+        .collect()
+}
+
+// Part 2
+fn incomplete_lines_in(text: &str) -> Vec<IncompleteLine> {
+    text.lines()
+        .map(|l| l.trim())
+        .filter_map(|line| match parse_line(line) {
+            ParseLineResult::Incomplete(incomplete_line) => Some(incomplete_line),
+            _ => None,
+        })
+        .collect()
+}
+
+// Part 1
 fn score_for(syntax_errors: &[SyntaxError]) -> usize {
     syntax_errors
         .iter()
@@ -88,6 +149,14 @@ fn score_for(syntax_errors: &[SyntaxError]) -> usize {
             _ => panic!("no score for symbol {:?}", c),
         })
         .sum()
+}
+
+// Part 2
+fn completion_score_of(incomplete_lines: &[IncompleteLine]) -> usize {
+    // We need to sort all the scores, and then select the middle score.
+    let mut scores: Vec<usize> = incomplete_lines.iter().map(|x| x.score()).collect();
+    scores.sort_unstable();
+    scores[scores.len() / 2]
 }
 
 #[cfg(test)]
@@ -108,8 +177,32 @@ mod test {
     ";
 
     #[test]
-    fn test_part1() {
+    fn test_part1_syntax_errors() {
         let score = score_for(&syntax_errors_in(INPUT));
         assert_eq!(score, 26397);
+    }
+
+    #[test]
+    fn test_part2_incomplete_lines() {
+        let completed_expected = "\
+            [({(<(())[]>[[{[]{<()<>>}}]])})]
+            [(()[<>])]({[<{<<[]>>()}>]})
+            (((({<>}<{<{<>}{[]{[]{}}}>}>))))
+            {<[[]]>}<{[{[{[]{()[[[]]]}}]}]}>
+            <{([{{}}[<[[[<>{}]]]>[]]])}>
+        ";
+        let completed_expected: Vec<&str> = completed_expected
+            .lines()
+            .map(|x| x.trim())
+            .filter(|x| !x.is_empty())
+            .collect();
+
+        let incomplete_lines = incomplete_lines_in(INPUT);
+        let completed_actual: Vec<String> =
+            incomplete_lines.iter().map(|x| x.completed()).collect();
+        assert_eq!(completed_actual, completed_expected);
+
+        let completion_score = completion_score_of(&incomplete_lines);
+        assert_eq!(completion_score, 288957);
     }
 }
